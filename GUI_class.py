058c8cf836ceaadca55fb456a426be7b0bc8ba31
federicodzf <federicodezabala@gmail.com>
2022-07-10 12:22:01 +0200

    Updated gui class

--- GUI_class.py
+++ GUI_class.py
@@ -9,3 +9,9 @@ class gui():
     def __init__(self):
         self.window = Tk()
         self.window.attributes('-fullscreen', True)
+
+def main():
+    gui = gui()
+
+if __name__ == "__main__":
+    main()
