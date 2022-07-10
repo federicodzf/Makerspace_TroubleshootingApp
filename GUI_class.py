ac968bde2addc1455781d5982906a9474d959c70
federicodzf <federicodezabala@gmail.com>
2022-07-10 12:18:05 +0200

    Created the GUI class and interface using tkinder

---
+++ GUI_class.py
@@ -0,0 +1,11 @@
+import os
+from tkinter import *
+from tkinter import messagebox
+import tkinter as tk
+import sys
+import datetime
+
+class gui():
+    def __init__(self):
+        self.window = Tk()
+        self.window.attributes('-fullscreen', True)
