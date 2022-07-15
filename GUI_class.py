587a7e18d1641f5c72032a7e2f96c5caa2b1c484
federicodzf <federicodezabala@gmail.com>
2022-07-15 13:34:25 +0200

    Added Yes and No buttons to window. No fucntionality yet. Added more pack() tutorials

--- GUI_class.py
+++ GUI_class.py
@@ -18,61 +18,82 @@ class gui():
         # Set standard size of window
         self.masterWindow.geometry("500x500+400+250")
         self.masterWindow.configure(bg=gui.bgColor)
-        ## Configure rows and columns in order to use .grid() and change weight of widgets 
-        self.masterWindow.columnconfigure(list(range(gui.cols)), minsize=75, weight=1)
-        self.masterWindow.rowconfigure(list(range(gui.rows)),minsize=50, weight=1)
 
         # Add Exit button
         self.addExitButton()
-        ## Add introductory text
-        self.createTextFrame("Encountering issues with a printer?")
+        # Add text frame
+        self.createTextFrame()
+        # Add intiial basic question text
+        self.addQuestion("Does the printer turn on?")
+        # Add button frame 
+        self.createButtonFrame()
+
+        self.addYesBtn()
+        self.addNoBtn()
+
         ## Make program event-driven
         self.masterWindow.mainloop()
  
     def addExitButton(self):
         # create exit btn frameconda 
-        self.exitBtnFrame = tk.Frame(self.masterWindow,bg="red")
-        self.exitBtnFrame.grid(row=gui.rows-1,columnspan=gui.cols,sticky="se",padx=10,pady=10)
+        self.exitBtnFrame = tk.Frame(self.masterWindow,bg="green")
+        self.exitBtnFrame.pack(side=BOTTOM, anchor=E)
         # Exit button 
-        self.exit_btn = tk.Button(self.exitBtnFrame, text="Exit", command=self.quitWindow,relief=tk.RAISED)
-        self.exit_btn.grid(row=gui.rows-1, column=gui.cols-1, sticky="se", padx=5, pady=5)
+        self.exit_btn = tk.Button(self.exitBtnFrame, text="Exit", command=self.quitWindow,relief=tk.RAISED, bg="red")
+        self.exit_btn.pack(anchor=W,side=RIGHT, ipadx=10, ipady=10)
         self.masterWindow.protocol("WM_DELETE_WINDOW", self.quitWindow) 
         
     def quitWindow(self):
         # Need to ask client if they are sure they want to exit
         self.masterWindow.destroy() # close window
 
-    def createTextFrame(self,msg):
+    def createTextFrame(self):
         self.txtFrame = tk.Frame(
             master=self.masterWindow,
-            height=100,
-            bg=gui.bgColor)
-        self.txtFrame.grid(row=0,columnspan=10,sticky='n')
+            height=200,
+            bg=gui.bgColor,
+            padx=20, pady=20)
+        self.txtFrame.pack(side=TOP, fill=BOTH)
+
+    def addQuestion(self,msg):
         # Create Intro message
         self.lbl_intro = tk.Label(
             master=self.txtFrame,
-            relief = tk.RAISED,
             anchor='w',
             text=msg,
-            font=(100),
-            fg='black')
-        self.lbl_intro.grid(row=0,columnspan=gui.cols,padx=10,pady=10)
-        
-    def yesButtonWidget(self,question):
-        # Parent widget for the buttons
+            font=(120),
+            fg='black',
+            bg=gui.bgColor)
+        self.lbl_intro.pack()
+
+    # Method that creates a button Frame
+    def createButtonFrame(self):
         self.buttons_frame = Frame(
             master=self.masterWindow,
-            height=200,
-            bg="yellow")
+            height=100,
+            bg="green",)
+        self.buttons_frame.pack(fill=BOTH)
+
+    def addYesBtn(self):
+        # Add Yes button to frame
         self.yesBtn = tk.Button(
             master=self.buttons_frame,
-            text=question,
+            text="Yes",
             fg="black",
-            anchor=CENTER,
-            padx=10,pady=10)
-        self
+            anchor=CENTER)
+        self.yesBtn.pack(side=LEFT,fill=BOTH,ipadx=10, ipady=10, padx=10, pady=10)
 
-        
+    def addNoBtn(self):
+        # Add No button to button frame
+        self.noBtn = tk.Button(
+            master=self.buttons_frame,
+            text="No",
+            fg="black",
+            anchor=CENTER)
+        self.noBtn.pack(side=RIGHT,fill=BOTH,ipadx=10, ipady=10, padx=10, pady=10)
+
+
 
 if __name__ == "__main__":
     ui = gui()
+    
